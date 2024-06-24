400cbad6033a03e757fb93dcf48a95d71a4259b7
techvijayprem <147999397+techvijayprem@users.noreply.github.com>
2024-06-24 09:21:09 +0530

    Add files via upload

---
+++ deuque/src/deuque/deuqu.java
@@ -0,0 +1,27 @@
+package deuque;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class deuqu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Deque<Integer> dq = new ArrayDeque<Integer>();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<5; i++)
+        {
+        	int no = sc.nextInt();
+        	
+        }
+		dq.add(10);
+		dq.add(20);
+		
+		System.out.println(dq);
+
+	}
+
+}
